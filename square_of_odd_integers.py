3442ee199a63d08352f2c079b91c8cb1e47b5166
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-08 21:01:47 +0800

    Rephrase comments for readability and descriptive reasons.

--- square_of_odd_integers.py
+++ square_of_odd_integers.py
@@ -3,8 +3,8 @@
 # Make a list
 list = [4, 5, 14, 14]
 
-# Iterate through the list
-squared_list = [element **2 for element in list if element % 2 == 1]
+# Find the odd elements and square them using list comprehension.
+squared_list = [element ** 2 for element in list if element % 2 == 1]
 
-# Print the square of the odd element
+# Print the square of the odd elements that can be found
 print(squared_list)

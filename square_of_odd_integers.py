9457f0d2a35d4ef2c641dee59eccfc9d74a54fa9
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-08 20:42:10 +0800

    Add pseudocode.

---
+++ square_of_odd_integers.py
@@ -0,0 +1,16 @@
+'''
+from a given list of integers, create a new list using comprehension that will compute the square of odd integer elements.
+
+sample calls
+
+[2,4,3] == [9]
+[0,0,1,1] == [1,1]
+
+'''
+
+# pseudocode
+
+# make a list
+# iterate through the list
+# check if the element is odd
+# print the square of the odd element

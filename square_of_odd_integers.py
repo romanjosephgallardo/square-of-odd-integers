327400f228378dae679317e291bc68fcd95bf01f
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-08 21:48:08 +0800

    Transform into OOP.

--- square_of_odd_integers.py
+++ square_of_odd_integers.py
@@ -1,10 +1,21 @@
 # Square of Odd Integers from a list
 
 # Make a list
-list = [4, 5, 14, 14]
+list_1 = [2, 4, 3]
+list_2 = [0, 0, 1, 1]
+list_3 = [4, 5, 14, 14, 1, 4, 18, 9, 1, 14]
 
 # Find the odd elements and square them using list comprehension.
-squared_list = [element ** 2 for element in list if element % 2 == 1]
+class SquareOfOddIntegers:
+    def __init__(self):
+        pass
+
+    def computation(self, list):
+        squared_list = [element ** 2 for element in list if element % 2 == 1]
+        return squared_list
 
 # Print the square of the odd elements that can be found
-print(squared_list)
+square_of_odd_elements = SquareOfOddIntegers()
+print(square_of_odd_elements.computation(list_1))
+print(square_of_odd_elements.computation(list_2))
+print(square_of_odd_elements.computation(list_3))
